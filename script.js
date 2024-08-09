071333cfc51a4f46ef0f2b421266b4640169cb0c
Nguyen Phuc Dinh Quyen <npdquyen@gmail.com>
2024-08-09 09:58:24 +0700

    CounterTimer

--- script.js
+++ script.js
@@ -1,18 +1,59 @@
+const sec = document.getElementById("secCounter");
+const min = document.getElementById("minCounter");
+const minlbl = document.getElementById("minlbl");
+const seclbl = document.getElementById("seclbl");
+const btn = document.getElementById("btn");
 
-const increasing = document.getElementById("inc");
-const decreasing = document.getElementById("dec");
-const reset = document.getElementById("res");
-const label = document.getElementById("label");
-let count = 0;
-increasing.onclick = function(){
-    count++;
-    label.textContent = count;
+let intervalId; 
+let isRunning = false;
+
+function updateMinutes(m){
+  let minNum= String(m);
+  if(minNum.length < 2) minNum = "0" + minNum;
+  minlbl.textContent = minNum;
 }
-decreasing.onclick = function(){
-    count--;
-    label.textContent = count;
+
+function updateSeconds(s){
+  let secNum = String(s);
+  if(secNum.length < 2) secNum = "0" + secNum;
+  seclbl.textContent = secNum;
 }
-reset.onclick = function(){
-    count = 0;
-    label.textContent = count;
+
+min.addEventListener('change',() => {
+  updateMinutes(min.value);
+});
+sec.addEventListener('change',() => {
+  updateSeconds(sec.value);
+});
+
+function countdown(min,sec) {
+
+  intervalId = setInterval(() => {
+    updateSeconds(sec);
+    updateMinutes(min);
+    sec--;
+    if (sec < 0 ) {
+      if(min > 0){
+        min -= 1;
+        sec += 60;
+      }
+      else{
+      clearInterval(intervalId);
+      }
+    }
+  }, 1000);
 }
+
+
+btn.onclick = function() {
+  if(!isRunning){
+    countdown(min.value,sec.value);
+    isRunning = true;
+  }
+  else{
+    clearInterval(intervalId);
+    updateMinutes(0);
+    updateSeconds(0);
+    isRunning = false;
+  }
+};
